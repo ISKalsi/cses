c1521dcb5c2183b7cbd46ad1c1537e01ee9e252c
pavandubey <ikroop.singh.kalsi@gmail.com>
2021-06-23 23:54:54 +0530

    better readability

--- sorting_and_searching/restaurant_customers.cpp
+++ sorting_and_searching/restaurant_customers.cpp
@@ -1,4 +1,7 @@
 #include<bits/stdc++.h>
+#define ARRIVAL_OR_LEAVE 1
+#define ARRIVE 1
+#define LEAVE -1
 
 using namespace std;
 
@@ -8,19 +11,19 @@ int main() {
     freopen("output.txt", "w", stdout);
 #endif
 
-    set<array<int, 2>> s;
+    set<array<int, 2>> customerTimings;
     int n; cin >> n;
 
     while(n--) {
         int arrival, leave;
         cin >> arrival >> leave;
-        s.insert({arrival, 1});
-        s.insert({leave, -1});
+        customerTimings.insert({arrival, ARRIVE});
+        customerTimings.insert({leave, LEAVE});
     }
 
     int maxNumber = 0, currentNumber = 0;
-    for(auto i: s) {
-        currentNumber += i[1];
+    for(auto timing: customerTimings) {
+        currentNumber += timing[ARRIVAL_OR_LEAVE];
         maxNumber = max(currentNumber, maxNumber);
     }
 
